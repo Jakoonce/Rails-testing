3db4c2b8cc981f27380a7cd04fcbc9c8fe245bf9
Joe Koonce <jakoonce00100@gmail.com>
2019-04-25 10:58:46 -0400

    added sorcery authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+  
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
+  resources :authors
+
   resources :articles do
     resources :comments
   end
+
   resources :tags
   
 end
